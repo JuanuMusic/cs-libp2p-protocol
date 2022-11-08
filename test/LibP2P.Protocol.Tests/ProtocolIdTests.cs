65c754deea705a128cd95cc7fc68d7a6f7119dc1
Juanu <juanu@juanu.com.ar>
2022-11-08 13:38:52 +0100

    Migrated to net_standerd 2.1
    All test passing.
    
    Also migrated Xunit to NUnit

--- test/LibP2P.Protocol.Tests/ProtocolIdTests.cs
+++ test/LibP2P.Protocol.Tests/ProtocolIdTests.cs
@@ -2,55 +2,55 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
+using NUnit.Framework;
 
 namespace LibP2P.Protocol.Tests
 {
     public class ProtocolIdTests
     {
-        [Fact]
+        [Test]
         public void Constructor_GivenProtocol_ReturnsValidProtocolId()
         {
             var pid = new ProtocolId("/test/pid");
 
-            Assert.Equal(pid.ToString(), "/test/pid");
+            Assert.AreEqual(pid.ToString(), "/test/pid");
         }
 
-        [Fact]
+        [Test]
         public void Equals_GivenTwoEqualIds_ReturnsEquality()
         {
             var pid1 = new ProtocolId("/test/pid");
             var pid2 = new ProtocolId("/test/pid");
 
-            Assert.Equal(pid1, pid2);
+            Assert.AreEqual(pid1, pid2);
         }
 
-        [Fact]
+        [Test]
         public void Equals_GivenTwoNotEqualIds_ReturnsNoEquality()
         {
             var pid1 = new ProtocolId("/test/pid1");
             var pid2 = new ProtocolId("/test/pid2");
 
-            Assert.NotEqual(pid1, pid2);
+            Assert.AreNotEqual(pid1, pid2);
         }
 
-        [Fact]
+        [Test]
         public void Cast_GivenString_ReturnsProtocolId()
         {
             var pids = "/test/pid";
             var pid = (ProtocolId) pids;
 
-            Assert.IsType<ProtocolId>(pid);
-            Assert.Equal(pid.ToString(), pids);
+            Assert.IsInstanceOf<ProtocolId>(pid);
+            Assert.AreEqual(pid.ToString(), pids);
         }
 
-        [Fact]
+        [Test]
         public void Cast_GivenProtocolId_ReturnsString()
         {
             var pid = new ProtocolId("/test/pid");
             var pids = (string) pid;
 
-            Assert.Equal(pids, pid.ToString());
+            Assert.AreEqual(pids, pid.ToString());
         }
     }
 }
